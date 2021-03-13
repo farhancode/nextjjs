05065e167f83b471424e26d356a675303963f153
Farhan <48584133+farhancode@users.noreply.github.com>
2021-03-13 19:45:02 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">My World!</a>
+          Welcome to <a href="https://imgur.com/gallery/FI9Y1Se/">My World!</a>
         </h1>
 
         <p className={styles.description}>
